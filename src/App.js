130859043b280c78fa217f57c4b6ff912fc1c2c1
Maher Al-Rifaï <maher.refae15@gmail.com>
2023-03-19 19:13:30 +0200

    maher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Cover from './components/cover/Cover';
 import Navbar from './components/navbar/Navbar';
+import SideNav from './components/navbar/SideNav';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SideNav/>
       <Cover/>
     </div>
   );
--- src/components/navbar/SideNav.js
+++ src/components/navbar/SideNav.js
@@ -1,20 +1,26 @@
-import classes from "./Navbar.module.css";
+import classes from "./SideNav.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 // import Dlogo from "../../assets/logo-dark.png"
 import Llogo from "../../assets/logo-light.png";
-const Navbar = () => {
+
+const SideNav = () => {
   return (
     <div className={classes.navbar}>
       <div className={classes.logo}>
         <img src={Llogo} alt="logo" />
       </div>
-      <div className={classes.links}>
+      <div className={classes.icon}>
+      <FontAwesomeIcon icon={faBars} className="fa-lg"/>
+      </div>
+      {/* <div className={classes.links}>
         <a href="#home">home</a>
         <a href="#about">about</a>
         <a href="">services</a>
         <a href="">projects</a>
         <a href="">contact</a>
-      </div>
+      </div> */}
     </div>
   );
 };
-export default Navbar;
+export default SideNav;
